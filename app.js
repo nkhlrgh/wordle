720bf9f224da122d4a282fb7cb00aa601919a69d
nkhlrgh <43105834+nkhlrgh@users.noreply.github.com>
2022-02-17 22:18:05 +0530

    Create guess boxes

--- app.js
+++ app.js
@@ -32,6 +32,30 @@ const keys = [
   "«",
 ];
 
+const guessRows = [
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+];
+
+guessRows.forEach((guessRow, guessRowIndex) => {
+  const rowElement = document.createElement("div");
+  rowElement.setAttribute("id", "guessrow-" + guessRowIndex);
+  guessRow.forEach((guess, guessIndex) => {
+    const tileElement = document.createElement("div");
+    tileElement.setAttribute(
+      "id",
+      "guessrow-" + guessRowIndex + "-tile-" + guessIndex
+    );
+    tileElement.classList.add("tile");
+    rowElement.append(tileElement);
+  });
+  tileDisplay.append(rowElement);
+});
+
 const handleClick = () => {
   console.log("clicked");
 };
