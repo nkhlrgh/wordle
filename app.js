0048e69dba9214c0f5619114c181aa1978244971
nkhlrgh <43105834+nkhlrgh@users.noreply.github.com>
2022-02-17 23:09:37 +0530

    Add word check

--- app.js
+++ app.js
@@ -45,6 +45,7 @@ const guessRows = [
 
 let currentRow = 0;
 let currentTile = 0;
+let isGameOver = false;
 
 guessRows.forEach((guessRow, guessRowIndex) => {
   const rowElement = document.createElement("div");
@@ -109,10 +110,22 @@ const deleteLetter = () => {
 const checkRow = () => {
   const guess = guessRows[currentRow].join("");
 
-  if (currentTile === 5) {
+  if (currentTile > 4) {
     console.log("guess is " + guess + " wordle is " + wordle);
     if (wordle == guess) {
       showMessage("Magnificent!");
+      isGameOver = true;
+      return;
+    } else {
+      if (currentRow >= 5) {
+        isGameOver = true;
+        showMessage("Game Over!");
+        return;
+      }
+      if (currentRow < 5) {
+        currentRow++;
+        currentTile = 0;
+      }
     }
   }
 };
